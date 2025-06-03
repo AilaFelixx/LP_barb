85f3164ff5c77649365fc76e87e7727de985fb4a
AilaFelixx <Aila_camilly@yahoo.com>
2025-06-02 21:19:41 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById('ull')
+
+function menuHamb() {
+    if (ull.style.display === 'none') {
+        ull.style.display = 'flex'
+    } else {
+        ull.style.display = 'none'    
+    }
+}
